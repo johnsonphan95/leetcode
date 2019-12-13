f835f612edb0dc3e6a8befbf16cc6ac3c322680e
Johnson <johnsonphan95@gmail.com>
2019-12-13 15:48:47 -0800

    Complete leetcode #3

---
+++ leetcode_problems/3_longest_substring_without_repeating_characters.js
@@ -0,0 +1,17 @@
+// Given a string, find the length of the longest substring without repeating characters.
+
+var lengthOfLongestSubstring = function(s) {
+    let max = 0; 
+    let start = 0; 
+    let seen = {};
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (seen[char] >= start) {
+            start = seen[char] + 1;
+        }
+        max = Math.max(max, i - start + 1);
+        seen[char] = i;
+    }
+    return max;
+};
