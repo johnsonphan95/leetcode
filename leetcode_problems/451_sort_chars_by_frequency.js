0d283552d3d9337fbb1d5d2cfac360df6099a1fd
Johnson <johnsonphan95@gmail.com>
2020-02-09 19:26:37 -0800

    lc 451

--- leetcode_problems/451_sort_chars_by_frequency.js
+++ leetcode_problems/451_sort_chars_by_frequency.js
@@ -1,30 +1,18 @@
 // Given a string, sort it in decreasing order based on the frequency of characters.
 
 var frequencySort = function(s) {
-  const map = new Map();
-
-  for (let i = 0; i < s.length; i++) {
-    map.set(s[i], (map.get(s[i]) || 0) + 1);
+  let map = {}; 
+  let chars = s.split("");
+  let result = "";
+  
+  for (const char of chars) {
+      map[char] = (map[char] || 0) + 1;
   }
-
-  const result = [];
-
-  for (let key of map.keys()) {
-    result.push([key, map.get(key)]);
-  }
-
-  result.sort((a, b) => b[1] - a[1]);
-
-  let resultStr = "";
-
-  for (let i = 0; i < result.length; i++) {
-    let j = 0;
-
-    while (j < result[i][1]) {
-      resultStr += result[i][0];
-      j++;
-    }
-  }
-
-  return resultStr;
+  
+  Object.keys(map).sort((a, b) => map[b] - map[a]).forEach(char => {
+      for (let i = map[char]; i > 0; i--) {
+          result += char;
+      }
+  })
+  return result;
 };
