e87a53c8b319d0ca2261472e1e3b1ea26177affb
Johnson <johnsonphan95@gmail.com>
2020-05-08 22:33:17 -0500

    lc 81

--- leetcode_problems/81_search_in_rotated_array.js
+++ leetcode_problems/81_search_in_rotated_array.js
@@ -12,3 +12,26 @@
 
 // Input: nums = [2,5,6,0,0,1,2], target = 3
 // Output: false
+
+var search = function(nums, target) {
+  let lo = 0;
+  let hi = nums.length - 1;
+
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+
+    if (target === nums[mid]) return true; 
+
+    while (nums[mid] == nums[hi] && mid != hi) hi--;
+
+    if (nums[mid > nums[i]]) {
+      if (target < nums[mid] && target >= nums[lo]) hi = mid - 1; 
+      else lo = mid + 1;
+    } else {
+      if (target > nums[mid] && target <= nums[lo]) lo = mid + 1; 
+      else hi = mid - 1;
+    }
+  }
+  
+  return false;
+};
